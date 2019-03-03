eee5bea1f196e172a694a467165928eea11bb659
BuShuanshuan <bushuanshuan@BuShuanshuandeMacBook-Pro.local>
2019-03-03 13:03:56 +0800

    basic version

--- sudo_tool.py
+++ sudo_tool.py
@@ -1,6 +1,33 @@
 # coding=utf-8
 
 import paramiko
-import os
+import threading
 
-print "hello"
+def remote_target_execute(ip,port,username,passwd,command):
+
+	ssh_session = paramiko.SSHClient()
+	ssh_session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh_session.connect(ip,port,username,passwd)
+
+	stdin, stdout, stderr = ssh_session.exec_command(command)
+
+	result = stdout.readlines()
+	for line in result:
+		print line,
+
+	ssh_session.close()
+
+def main():
+
+	ip = "192.168.2.109"
+	port = 22
+	username = "root"
+	passwd = "carrot"
+
+	command = "ls"
+	
+	remote_target_execute(ip,port,username,passwd,command)
+
+
+if __name__ == '__main__':
+	main()
